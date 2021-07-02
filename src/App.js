4b58119821c7d27547bac70c76749578cdce6a7d
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-07-02 22:13:21 +0300

    написал 1 задание

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import user from './json-files/user.json';
+import Profile from './components/Profile';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import user from '../json-files/user.json';
+
+const Profile = ({ name, avatar, tag, location, stats}) => {
+
+   return <div class="profile" key={name}>
+        <div class="description">
+            <img
+            src={avatar}
+            alt="Аватар пользователя"
+            class="avatar"
+            />
+            <p class="name">{name}</p>
+            <p class="tag">{tag}</p>
+            <p class="location">{location}</p>
+        </div>
+
+        <ul class="stats">
+            <li>
+            <span class="label">Followers</span>
+            <span class="quantity">{stats.followers}</span>
+            </li>
+            <li>
+            <span class="label">Views</span>
+            <span class="quantity">{stats.views}</span>
+            </li>
+            <li>
+            <span class="label">Likes</span>
+            <span class="quantity">{stats.likes}</span>
+            </li>
+        </ul>
+
+    </div>
+};
+
+Profile.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    stats: PropTypes.string,
+}
+
+export default Profile;
